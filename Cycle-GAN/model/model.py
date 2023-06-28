8fc61c2df48e80cc858fca7cceee376f5251f4d6
Wenhui <63658823+ChongQingNoSubway@users.noreply.github.com>
2023-06-28 13:51:52 -0700

    Add files via upload

---
+++ Cycle-GAN/model/model.py
@@ -0,0 +1,250 @@
+from cv2 import reduce
+import torch 
+import torch.nn as nn
+import math
+
+
+import torch
+import torch.nn as nn
+import math
+
+def conv(in_channels, out_channels, kernel_size, bias=False, stride = 1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2), bias=bias, stride = stride)
+
+class SAM(nn.Module):
+    def __init__(self, n_feat, kernel_size, bias):
+        super(SAM, self).__init__()
+        self.conv1 = conv(n_feat, n_feat, kernel_size, bias=bias)
+        self.conv2 = conv(n_feat, 3, kernel_size, bias=bias)
+        self.conv3 = conv(3, n_feat, kernel_size, bias=bias)
+
+    def forward(self, x, x_img):
+        x1 = self.conv1(x)
+        img = self.conv2(x) + x_img
+        x2 = torch.sigmoid(self.conv3(img))
+        x1 = x1*x2
+        x1 = x1+x
+        return x1, img
+
+##########################################################################
+## Channel Attention Layer
+class CALayer(nn.Module):
+    def __init__(self, channel, reduction=16, bias=False):
+        super(CALayer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+                nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=bias),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+class EfficientCALayer(nn.Module):
+    def __init__(self, k_size = 3):
+        super(EfficientCALayer,self).__init__()
+        self.avg_pooling = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1)//2, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self,x):
+
+        y = self.avg_pooling(x)
+
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+##########################################################################
+## Channel Attention Block (CAB)
+class CAB(nn.Module):
+    def __init__(self, n_feat, kernel_size, reduction, bias, act):
+        super(CAB, self).__init__()
+        modules_body = []
+        modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+        modules_body.append(act)
+        modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+
+        #self.CA = CALayer(n_feat, reduction, bias=bias)
+        self.CA = EfficientCALayer(reduction)
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res = self.CA(res)
+        res += x
+        return res
+
+class Encoder(nn.Module):
+    def __init__(self, n_feat, kernel_size, reduction, act, bias, scale_unetfeats, csff):
+        super(Encoder, self).__init__()
+
+        self.encoder_level1 = [CAB(n_feat,                     kernel_size, reduction[0], bias=bias, act=act) for _ in range(2)]
+        self.encoder_level2 = [CAB(n_feat+scale_unetfeats,     kernel_size, reduction[1], bias=bias, act=act) for _ in range(2)]
+        self.encoder_level3 = [CAB(n_feat+(scale_unetfeats*2), kernel_size, reduction[2], bias=bias, act=act) for _ in range(2)]
+
+        self.encoder_level1 = nn.Sequential(*self.encoder_level1)
+        self.encoder_level2 = nn.Sequential(*self.encoder_level2)
+        self.encoder_level3 = nn.Sequential(*self.encoder_level3)
+
+        self.down12  = DownSample(n_feat, scale_unetfeats)
+        self.down23  = DownSample(n_feat+scale_unetfeats, scale_unetfeats)
+
+        # Cross Stage Feature Fusion (CSFF)
+        if csff:
+            self.csff_enc1 = nn.Conv2d(n_feat,                     n_feat,                     kernel_size=1, bias=bias)
+            self.csff_enc2 = nn.Conv2d(n_feat+scale_unetfeats,     n_feat+scale_unetfeats,     kernel_size=1, bias=bias)
+            self.csff_enc3 = nn.Conv2d(n_feat+(scale_unetfeats*2), n_feat+(scale_unetfeats*2), kernel_size=1, bias=bias)
+
+            self.csff_dec1 = nn.Conv2d(n_feat,                     n_feat,                     kernel_size=1, bias=bias)
+            self.csff_dec2 = nn.Conv2d(n_feat+scale_unetfeats,     n_feat+scale_unetfeats,     kernel_size=1, bias=bias)
+            self.csff_dec3 = nn.Conv2d(n_feat+(scale_unetfeats*2), n_feat+(scale_unetfeats*2), kernel_size=1, bias=bias)
+
+    def forward(self, x, encoder_outs=None, decoder_outs=None):
+        enc1 = self.encoder_level1(x)
+        if (encoder_outs is not None) and (decoder_outs is not None):
+            enc1 = enc1 + self.csff_enc1(encoder_outs[0]) + self.csff_dec1(decoder_outs[0])
+
+        x = self.down12(enc1)
+
+        enc2 = self.encoder_level2(x)
+        if (encoder_outs is not None) and (decoder_outs is not None):
+            enc2 = enc2 + self.csff_enc2(encoder_outs[1]) + self.csff_dec2(decoder_outs[1])
+
+        x = self.down23(enc2)
+
+        enc3 = self.encoder_level3(x)
+        if (encoder_outs is not None) and (decoder_outs is not None):
+            enc3 = enc3 + self.csff_enc3(encoder_outs[2]) + self.csff_dec3(decoder_outs[2])
+        
+        return [enc1, enc2, enc3]
+
+class Decoder(nn.Module):
+    def __init__(self, n_feat, kernel_size, reduction, act, bias, scale_unetfeats):
+        super(Decoder, self).__init__()
+
+        self.decoder_level1 = [CAB(n_feat,                     kernel_size, reduction[2], bias=bias, act=act) for _ in range(2)]
+        self.decoder_level2 = [CAB(n_feat+scale_unetfeats,     kernel_size, reduction[1], bias=bias, act=act) for _ in range(2)]
+        self.decoder_level3 = [CAB(n_feat+(scale_unetfeats*2), kernel_size, reduction[0], bias=bias, act=act) for _ in range(2)]
+
+        self.decoder_level1 = nn.Sequential(*self.decoder_level1)
+        self.decoder_level2 = nn.Sequential(*self.decoder_level2)
+        self.decoder_level3 = nn.Sequential(*self.decoder_level3)
+
+        self.skip_attn1 = CAB(n_feat,                 kernel_size, reduction[2], bias=bias, act=act)
+        self.skip_attn2 = CAB(n_feat+scale_unetfeats, kernel_size, reduction[1], bias=bias, act=act)
+
+        self.up21  = SkipUpSample(n_feat, scale_unetfeats)
+        self.up32  = SkipUpSample(n_feat+scale_unetfeats, scale_unetfeats)
+
+    def forward(self, outs):
+        enc1, enc2, enc3 = outs
+        dec3 = self.decoder_level3(enc3)
+
+        x = self.up32(dec3, self.skip_attn2(enc2))
+        dec2 = self.decoder_level2(x)
+
+        x = self.up21(dec2, self.skip_attn1(enc1))
+        dec1 = self.decoder_level1(x)
+
+        return [dec1,dec2,dec3]
+
+class DownSample(nn.Module):
+    def __init__(self, in_channels,s_factor):
+        super(DownSample, self).__init__()
+        self.down = nn.Sequential(nn.Upsample(scale_factor=0.5, mode='bilinear', align_corners=False),
+                                  nn.Conv2d(in_channels, in_channels+s_factor, 1, stride=1, padding=0, bias=False))
+
+    def forward(self, x):
+        x = self.down(x)
+        return x
+
+class UpSample(nn.Module):
+    def __init__(self, in_channels,s_factor):
+        super(UpSample, self).__init__()
+        self.up = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
+                                nn.Conv2d(in_channels+s_factor, in_channels, 1, stride=1, padding=0, bias=False))
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+class SkipUpSample(nn.Module):
+    def __init__(self, in_channels,s_factor):
+        super(SkipUpSample, self).__init__()
+        self.up = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
+                                nn.Conv2d(in_channels+s_factor, in_channels, 1, stride=1, padding=0, bias=False))
+
+    def forward(self, x, y):
+        x = self.up(x)
+        x = x + y
+        return x
+
+class _NetG(nn.Module):
+    def __init__(self, in_c=3, out_c=3, n_feat=80, scale_unetfeats=48, scale_orsnetfeats=32, num_cab=8, kernel_size=3, reduction=[3,5,7], bias=False):
+        super(_NetG, self).__init__()
+
+        act=nn.PReLU()
+        self.shallow_feat1 = nn.Sequential(conv(3, n_feat, kernel_size, bias=bias), CAB(n_feat,kernel_size, 3, bias=bias, act=act))
+
+        # Cross Stage Feature Fusion (CSFF)
+        self.stage1_encoder = Encoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats, csff=False)
+        self.stage1_decoder = Decoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats)
+
+
+        self.sam12 = SAM(n_feat, kernel_size=1, bias=bias)
+
+
+    def forward(self, x3_img):
+        H = x3_img.size(2)
+        W = x3_img.size(3)
+
+        # Multi-Patch Hierarchy: Split Image into four non-overlapping patches
+
+        fea = self.shallow_feat1(x3_img)
+        fea2 = self.stage1_encoder(fea)
+        fea3 = self.stage1_decoder(fea2)
+        _, stage1_img = self.sam12(fea3[0], x3_img) 
+
+        return stage1_img
+
+
+class _NetD(nn.Module):
+    def __init__(self):
+        super(_NetD, self).__init__()
+
+        #channels, height, width = input_shape
+
+        # Calculate output shape of image discriminator (PatchGAN)
+        self.output_shape = (1, 256 // 2 ** 4, 256 // 2 ** 4)
+
+        def discriminator_block(in_filters, out_filters, normalize=True):
+                """Returns downsampling layers of each discriminator block"""
+                layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+                if normalize:
+                    layers.append(nn.InstanceNorm2d(out_filters))
+                layers.append(nn.LeakyReLU(0.2, inplace=True))
+                return layers
+        self.model = nn.Sequential(
+            *discriminator_block(3, 64, normalize=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(512, 1, 4, padding=1)
+        )
+
+
+    def forward(self, input):
+
+
+        return self.model(input)
---
+++ Cycle-GAN/train_full.py
@@ -0,0 +1,347 @@
+import argparse, os, glob
+import torch,pdb
+import math, random, time
+import numpy as np
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from model.model import _NetG,_NetD
+#from dataset_dep import DatasetFromHdf5
+from torchvision.utils import save_image
+import torch.utils.model_zoo as model_zoo
+from random import randint, seed
+import random
+import cv2
+#from dataloader.EyeQ import EyeQ_Dataset
+from dataloader.EyeQ_full import EyeQ_Dataset
+import albumentations as A 
+from albumentations.pytorch import ToTensorV2
+import torchvision.transforms as T
+import itertools
+
+# Training settings
+parser = argparse.ArgumentParser(description="PyTorch SRResNet") 
+parser.add_argument("--batchSize", type=int, default=4, help="training batch size")
+parser.add_argument("--nEpochs", type=int, default=200, help="number of epochs to train for")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
+parser.add_argument("--step", type=int, default=100, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=500")
+parser.add_argument("--cuda", default=True, help="Use cuda?")
+parser.add_argument("--resume", default="", type=str, help="Path to resume model (default: none")
+parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
+parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, (default: 1)")
+parser.add_argument("--pretrained", default="", type=str, help="Path to pretrained model (default: none)")
+parser.add_argument("--noise_sigma", default=50, type=int, help="standard deviation of the Gaussian noise (default: 50)")
+parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+parser.add_argument("--sigma", default=30, type=int)
+parser.add_argument("--num_rand",default=[1000,1000], type=list)
+parser.add_argument("--root", default="dataset/degratation/pre", type=str)
+parser.add_argument("--file_dir",default="dataset/train.csv", type=str)
+
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return total_num, trainable_num
+
+def main():
+    global opt, model, netContent
+    opt = parser.parse_args()
+    print(opt)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    cuda = opt.cuda
+    # if cuda: 
+    #     print("=> use gpu id: '{}'".format(opt.gpus))
+    #     os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpus
+    #     if not torch.cuda.is_available():
+    #             raise Exception("No GPU found or Wrong gpu id, please run without --cuda")
+
+    opt.seed = random.randint(1, 10000)
+    print("Random Seed: ", opt.seed)
+    torch.manual_seed(opt.seed)
+    if cuda:
+        torch.cuda.manual_seed(opt.seed)
+
+    cudnn.benchmark = True
+
+    print("===> Loading datasets")
+    # data_list = glob.glob(opt.trainset+"*.h5")
+    num_random = opt.num_rand
+    print("===> Building model")
+    g_AB = _NetG()
+    g_BA = _NetG()
+    discr_A = _NetD()
+    discr_B = _NetD()
+
+    criterion_GAN = torch.nn.MSELoss()
+    criterion_cycle = torch.nn.L1Loss()
+    criterion_identity = torch.nn.L1Loss()
+    #网络参数数量
+    # a,b=get_parameter_number(model)
+    # print(model)
+    # print(a,b)
+    print("===> Setting GPU")
+    if cuda:
+        #model = model.cuda()
+        #discr = discr.cuda()
+        if torch.cuda.device_count() > 1:
+            print("Let's use", torch.cuda.device_count(), "GPUs!")
+        #  dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+            g_AB = nn.DataParallel(g_AB)
+            g_BA = nn.DataParallel(g_BA)
+            discr_A = nn.DataParallel(discr_A)
+            discr_B = nn.DataParallel(discr_B)
+
+        g_AB.to(device=device)
+        g_BA.to(device=device)
+        discr_A.to(device=device)
+        discr_B.to(device=device)
+
+        #discr.to(device=device)
+        criterion_GAN = criterion_GAN.cuda()
+        criterion_cycle = criterion_cycle.cuda()
+        criterion_identity = criterion_identity.cuda()
+        
+
+    # optionally resume from a checkpoint
+    # if opt.resume:
+    #     if os.path.isfile(opt.resume):
+    #         print("=> loading checkpoint '{}'".format(opt.resume))
+    #         checkpoint = torch.load(opt.resume)
+    #         opt.start_epoch = checkpoint["epoch"] + 1
+    #         model.load_state_dict(checkpoint["model"].state_dict())
+    #         discr.load_state_dict(checkpoint["discr"].state_dict())
+    #     else:
+    #         print("=> no checkpoint found at '{}'".format(opt.resume))
+
+    # # optionally copy weights from a checkpoint
+    # if opt.pretrained:
+    #     if os.path.isfile(opt.pretrained):
+    #         print("=> loading model '{}'".format(opt.pretrained))
+    #         weights = torch.load(opt.pretrained)
+    #         model.load_state_dict(weights['model'].state_dict())
+    #         discr.load_state_dict(weights['discr'].state_dict())
+    #     else:
+    #         print("=> no model found at '{}'".format(opt.pretrained))
+
+    data_transforms = {
+        'HQ': T.Compose([
+                T.Resize((256,256)),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                #T.RandomRotation((-180,180)),
+                T.ToTensor()
+        ]),
+        'LQ': T.Compose([
+                T.Resize((256,256)),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                #T.RandomRotation((-180,180)),
+                T.ToTensor()
+                #T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    print("===> Setting Optimizer")
+    optimizer_G = torch.optim.RMSprop(
+        itertools.chain(g_BA.parameters(), g_AB.parameters()), lr=opt.lr/2
+    )
+
+    optimizer_D_A = torch.optim.RMSprop(discr_A.parameters(), lr=opt.lr)
+    optimizer_D_B = torch.optim.RMSprop(discr_B.parameters(), lr=opt.lr)    
+    #G_optimizer = optim.RMSprop(model.parameters(), lr=opt.lr/2)
+    #D_optimizer = optim.RMSprop(discr.parameters(), lr=opt.lr)
+
+    print("===> Training")
+    all_g =[]
+    all_d=[]
+    for epoch in range(opt.start_epoch, opt.nEpochs + 1):
+        g_loss = 0
+        d_loss = 0
+        num_rand = 0
+        for i in num_random:
+            train_set = EyeQ_Dataset(rootHQ=opt.root,rootPQ='dataset/degratation/deg',file_dir=opt.file_dir,select_number=i,transform_HQ=data_transforms['HQ'],transform_PQ=data_transforms['LQ'])
+            #train_set = EyeQ_Dataset(root=opt.root,file_dir=opt.file_dir,select_number=i,transform_HQ=data_transforms['HQ'],transform_PQ=data_transforms['LQ'])
+            #train_set = DatasetFromHdf5(data_name)
+            training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, \
+                batch_size=opt.batchSize, shuffle=True)
+            a,b=train(training_data_loader,criterion_GAN,criterion_cycle,criterion_identity, optimizer_G, optimizer_D_A,optimizer_D_B, g_AB,g_BA, discr_A, discr_B, epoch,num_rand)
+            g_loss += a
+            d_loss += b
+            num_rand += 1
+        g_loss = g_loss / len(num_random)
+        d_loss = d_loss / len(num_random)
+        all_g.append(format(g_loss))
+        all_d.append(format(d_loss))
+        save_checkpoint(g_AB, discr_A, epoch)
+
+        print(g_loss)
+
+    file = open('cycleGan/Experiment/exp_full/checksample/mse_'+str(opt.nEpochs)+'_'+str(opt.sigma)+'.txt','w')
+    for g in all_g:
+        file.write(g+'\n')
+    file.close()
+
+    file = open('cycleGan/Experiment/exp_full/checksample/Gloss_'+str(opt.nEpochs)+'_'+str(opt.sigma)+'.txt', 'w')
+    for d in all_d:
+        file.write(d + '\n')
+    file.close()
+    # psnr = eval_dep(model)
+    # print("Final psnr is:",psnr)
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10"""
+    lr = opt.lr * (0.1 ** (epoch // opt.step))
+    return lr 
+
+class ReplayBuffer:
+    def __init__(self, max_size=50):
+        assert max_size > 0, "Empty buffer or trying to create a black hole. Be careful."
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
+
+
+def train(training_data_loader,criterion_GAN,criterion_cycle,criterion_identity,optimizer_G, optimizer_D_A,optimizer_D_B, g_AB,g_BA, discr_A, discr_B, epoch,num_rand):
+    # training_data_loader, criterion_GAN,criterion_cycle,criterion_identity ,
+    # optimizer_G, optimizer_D_A,optimizer_D_B, g_AB,g_BA, discr_A, discr_B, epoch,num_rand
+    fake_A_buffer = ReplayBuffer()
+    fake_B_buffer = ReplayBuffer()
+
+    lr = adjust_learning_rate(optimizer_D_A, epoch-1)
+
+    all_loss_g = []
+    all_loss_d = []
+    for param_group in optimizer_G.param_groups:
+        param_group["lr"] = lr/2
+    for param_group in optimizer_D_B.param_groups:
+        param_group["lr"] = lr
+    for param_group in optimizer_D_A.param_groups:
+        param_group["lr"] = lr
+
+    print("Epoch={}, lr={}".format(epoch, optimizer_D_A.param_groups[0]["lr"]))
+    #model.train()
+    #discr.train()
+
+    for iteration, batch in enumerate(training_data_loader, 1):
+        
+        real_B = Variable(batch[1]) # B 
+        real_A = Variable(batch[0])    # A
+        #print(target)
+        
+        valid = Variable(torch.cuda.FloatTensor(np.ones((real_A.size(0), *discr_A.module.output_shape))), requires_grad=False)
+        fake = Variable(torch.cuda.FloatTensor(np.zeros((real_A.size(0), *discr_B.module.output_shape))), requires_grad=False)
+
+        if opt.cuda:
+            real_A = real_A.cuda()
+            real_B = real_B.cuda()
+
+        # train Generator
+        g_AB.train()
+        g_BA.train()
+
+        optimizer_G.zero_grad()
+
+        #output = g_BA(real_A)
+        #output_2 = g_AB(real_B)
+        loss_id_A = criterion_identity(g_BA(real_A),real_A)
+        loss_id_B = criterion_identity(g_AB(real_B),real_B)
+
+        loss_identity = (loss_id_A + loss_id_B) / 2
+        
+            # Gan loss
+        fake_B = g_AB(real_A)
+        loss_GAN_AB = criterion_GAN(discr_B(fake_B),valid)
+        fake_A = g_BA(real_B)
+        loss_GAN_BA = criterion_GAN(discr_A(fake_A),valid)
+
+
+        loss_GAN = (loss_GAN_AB + loss_GAN_BA) / 2
+
+            # cycle Loss
+        
+        recov_A = g_BA(fake_B)
+        loss_cycle_A = criterion_cycle(recov_A,real_A)
+        recov_B = g_AB(fake_A)
+        loss_cycle_B = criterion_cycle(recov_B,real_B)
+
+        loss_cycle = (loss_cycle_A + loss_cycle_B) / 2
+
+        # Totall Loss 
+        loss_G = loss_GAN + 10*loss_cycle + 5* loss_identity
+
+        all_loss_g.append(loss_G)
+
+        loss_G.backward()
+        optimizer_G.step()
+
+
+        # train discriminator A
+        optimizer_D_A.zero_grad()
+        # Real loss
+        
+        loss_real = criterion_GAN(discr_A(real_A), valid)
+        # Fake loss (on batch of previously generated samples)
+        fake_A_ = fake_A_buffer.push_and_pop(fake_A)
+        loss_fake = criterion_GAN(discr_A(fake_A_.detach()), fake)
+        # Total loss
+        loss_D_A = (loss_real + loss_fake) / 2
+
+        loss_D_A.backward()
+        optimizer_D_A.step()
+
+
+        # train discriminator B 
+        optimizer_D_B.zero_grad()
+
+        # Real loss
+        loss_real = criterion_GAN(discr_B(real_B), valid)
+        # Fake loss (on batch of previously generated samples)
+        fake_B_ = fake_B_buffer.push_and_pop(fake_B)
+        loss_fake = criterion_GAN(discr_B(fake_B_.detach()), fake)
+        # Total loss
+        loss_D_B = (loss_real + loss_fake) / 2
+
+        loss_D_B.backward()
+        optimizer_D_B.step()
+
+        loss_D = (loss_D_A + loss_D_B) / 2        
+        all_loss_d.append(loss_D)
+        
+        if iteration % 10 == 0:
+            print("===> Epoch[{}]({}/{}): Loss_D: {:.5}, loss_G: {:.5}".format(epoch, iteration, len(training_data_loader), loss_D.data, loss_G.data))
+    save_image(fake_B.data, 'cycleGan/Experiment/exp_full/checksample/'+str(epoch)+'_'+str(num_rand)+'_'+'output.png')
+    save_image(real_A.data, 'cycleGan/Experiment/exp_full/checksample/'+str(epoch)+'_'+str(num_rand)+'_'+'input.png')
+    save_image(real_B.data, 'cycleGan/Experiment/exp_full/checksample/'+str(epoch)+'_'+str(num_rand)+'_'+'gt.png')
+
+
+    return torch.mean(torch.FloatTensor(all_loss_g)),torch.mean(torch.FloatTensor(all_loss_d))
+   
+def save_checkpoint(model, discr, epoch):
+    model_out_path = "cycleGan/Experiment/exp_full/checkpoint/" + "model_denoise_"+str(epoch)+"_"+str(opt.sigma)+".pth"
+    state = {"epoch": epoch ,"model": model, "discr": discr}
+    if not os.path.exists("cycleGan/Experiment/exp_full/checkpoint/"):
+        os.makedirs("cycleGan/Experiment/exp_full/checkpoint/")
+
+    torch.save(state, model_out_path)
+
+    print("Checkpoint saved to {}".format(model_out_path))
+
+if __name__ == "__main__":
+    main()
